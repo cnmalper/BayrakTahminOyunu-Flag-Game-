46d82b8e72979a03ff4e93589957c0f67ffba698
Alper Canimoglu <teknoaydin24@gmail.com>
2023-03-16 12:18:24 +0300

    Initial Commit

---
+++ BayrakTahminOyunu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BayrakTahminOyunu
+//
+//  Created by Alper Canımoğlu on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
